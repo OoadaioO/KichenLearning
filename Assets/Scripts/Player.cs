70b0e76712272036e3aefe4a1a674d14e5645764
xubo <750983498@qq.com>
2024-04-18 09:43:18 +0800

    feat: tmp

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -65,6 +65,9 @@ public class Player : NetworkBehaviour, IKitchenObjectParent
 
     private void Update()
     {
+        if(!IsOwner){
+            return;
+        }
 
         HandleMovement();
         HandleInteractions();
